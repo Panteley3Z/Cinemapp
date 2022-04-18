1e9488ffe43267d95886ec24a1d1904468ebcd4a
Panteley <panteley3z@gmail.com>
2022-04-18 19:56:26 +0300

    Rebuild Added: - place for profile img on topPanel, - place for movie_cover, - place for movie_description, - button

--- app/src/main/java/ru/devzerkpant/cinemapp/MainActivity.kt
+++ app/src/main/java/ru/devzerkpant/cinemapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.devzerkpant.cinemapp
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_go.setOnClickListener {
+            Toast.makeText(this, "Enjoy watching the movie!", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
