de3a27d779fd0b0d17772f9697fef42ab67c5545
Panteley <panteley3z@gmail.com>
2022-04-23 20:16:35 +0300

    Added: - movie covers, - movie names, - bottom buttons - Drawable for backgrounds; - CardView; - Toast-messages-(quick descriptions)     for pressing on movie names.
    - activity_main x dimens
        fol landscape orientation.

--- app/src/main/java/ru/devzerkpant/cinemapp/MainActivity.kt
+++ app/src/main/java/ru/devzerkpant/cinemapp/MainActivity.kt
@@ -10,9 +10,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_watch.setOnClickListener {
-            Toast.makeText(this, "Enjoy watching the movie!", Toast.LENGTH_SHORT).show()
+        initButtons()
+    }
+
+    private fun initButtons() {
+        card_movie1desc.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Walter White, a chemistry teacher, discovers that he has cancer and decides to get into the meth-making business to repay his medical debts. His priorities begin to change when he partners with Jesse.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        card_movie2desc.setOnClickListener {
+            Toast.makeText(
+                this,
+                "The lives of four socially awkward friends, Leonard, Sheldon, Howard and Raj, take a wild turn when they meet the beautiful and free-spirited Penny.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        card_movie3desc.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Chuck Rhoades, a sincere but ruthless US attorney, engages in an egoistic battle with hedge fund kingpin Bobby 'Axe' Axelrod as they try to outdo each other in the competitive financial market.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        bottomP_button_marked.setOnClickListener {
+            Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show()
+        }
+        bottomP_button_wLater.setOnClickListener {
+            Toast.makeText(this, "Watch later", Toast.LENGTH_SHORT).show()
+        }
+        bottomP_button_fortune.setOnClickListener {
+            Toast.makeText(this, "Cinema fortune", Toast.LENGTH_SHORT).show()
         }
     }
+
 
 }
